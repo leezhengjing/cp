22cf67c207e48fe55b809b41dd92d5f987672975
leezhengjing <zhengjing.lee86@gmail.com>
2024-03-31 16:29:20 +0800

    Leetcode 17 - Letter Combinations of a Phone Number

--- leetcode/dfs/l17/l17.py
+++ leetcode/dfs/l17/l17.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         letters = [[],[],["a","b","c"],["d","e","f"],["g","h","i"],["j","k","l"],["m","n","o"],["p","q","r","s"],["t","u","v"],["w","x","y","z"]]
