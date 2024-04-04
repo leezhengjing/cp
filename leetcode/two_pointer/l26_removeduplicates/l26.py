d1c35aebe1e49542d428a3190727023c122b8c5f
leezhengjing <zhengjing.lee86@gmail.com>
2024-04-04 16:20:05 +0800

    Leetcode 26 - Remove Duplicates

---
+++ leetcode/two_pointer/l26_removeduplicates/l26.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+        return slow + 1
